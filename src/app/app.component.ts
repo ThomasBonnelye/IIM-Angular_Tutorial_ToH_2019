4e360c87880daa4b7e94e3ea130f59252c496ada
ThomasBONNELYE <thomas.bonnelye@devinci.fr>
2019-01-18 01:39:45 +0100

    master/detail components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'La ligue des légendes';
+  title = 'La Ligue des Légendes';
 }
