df6777e5bd219669e7e7faeb1acebef50127c6a8
ThomasBONNELYE <thomas.bonnelye@devinci.fr>
2019-01-18 01:34:18 +0100

    displaying a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Aatroce' },
+  { id: 12, name: 'Tamo' },
+  { id: 13, name: 'Fizz' },
+  { id: 14, name: 'Rammus' },
+  { id: 15, name: 'Neekotine' },
+  { id: 16, name: 'Rumble' },
+  { id: 17, name: 'Diana' },
+  { id: 18, name: 'Dr Mundo' },
+  { id: 19, name: 'Yasuo' },
+  { id: 20, name: 'Eclair Malade' }
+];
